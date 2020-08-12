6c5a2f3efd858b2b96ad2cff612552156afd5a56
wildan <wildanrgsspeed@gmail.com>
2020-08-12 13:05:32 +0700

    add 'button' components

---
+++ src/components/Button.svelte
@@ -0,0 +1,10 @@
+<style>
+  .bt-style {
+    padding: 1rem;
+    background-color: rgb(0, 162, 255);
+    color: white;
+    border-radius: 30rem;
+  }
+</style>
+
+<button class="bt-style">Coba Button</button>
